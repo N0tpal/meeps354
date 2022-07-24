3d1aaae5a3b6165af7db4da98c2c88a3ba96d1a2
onavvl <60692572+novaliore@users.noreply.github.com>
2022-07-24 11:14:30 +0700

    Update meeps354.php

--- meeps354.php
+++ meeps354.php
@@ -49,7 +49,7 @@ function change(){
                 echo "\n".color("blue"," [x1] DISKON 50% GoCar pake GoPay maks. Rp30.000");
                 sleep(1);
                 echo "\n".color("red"," [x1] DISKON GO-FOOD 50%!");
-                echo "\n\n".color("blue","              @ZaqiGans      ");
+                echo "\n\n".color("blue","              @meeps354      ");
                 echo "\n".color("white","      Silahkan Masuk ke VMOS/AppClone");
                 for($a=1;$a<=3;$a++){
                         echo color("white",".");
